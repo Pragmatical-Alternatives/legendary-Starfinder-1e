fb1cd23c27831a5d44bd6d4fba231f72ac835efc
ietzel <67884277+ietzel@users.noreply.github.com>
2025-04-15 16:25:03 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,9 @@ class Main {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);
     System.out.println("The story begins. Choose strength, agility, constitution, intelligence, wisdom, charisma (1-6).");
-
     String score = myObj.nextLine();
-    System.out.println("Username is: " + score);
+    System.out.println("Score: " + score);
+    System.out.println("There are many questions related to the crash landing which led to a sector civilization on an unexplored planet.");
+    System.out.println("Mysterious legends exist, such as of a desert with a primitive charred wood enclosure.");
   }
 }
