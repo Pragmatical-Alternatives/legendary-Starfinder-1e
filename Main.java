c8bcfdd22fc2587b4a979aec68cb7785c403ce07
ietzel <67884277+ietzel@users.noreply.github.com>
2025-04-16 11:17:35 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,9 +5,30 @@ class Main {
     Scanner myObj = new Scanner(System.in);
     System.out.println("The story begins. Choose strength, agility, constitution, intelligence, wisdom, charisma (1-6).");
     String score = myObj.nextLine();
-    System.out.println("Score: " + score);
     System.out.println("There are many questions related to the crash landing which led to a sector civilization on an unexplored planet.");
     System.out.println("Mysterious legends exist, such as of a desert with a primitive charred wood enclosure.");
     System.out.println("Coastal lifeforms speak of nature disturbed by the mystery.");
+    switch(score) {
+      case "1":
+          System.out.println("Melee & athletics.");
+          break;
+      case "2":
+          System.out.println("Ranged & acrobatics");
+          break;
+      case "3":
+          System.out.println("Kinetics & endurance");
+          break;
+      case "4":
+          System.out.println("Magic & knowledge");
+          break;
+      case "5":
+          System.out.println("Magic & oerception");
+          break;
+      case "6":
+          System.out.println("Magic & diplomacy");
+          break;
+      default:
+          System.out.println("Mixed approach.");
+    }
   }
 }
