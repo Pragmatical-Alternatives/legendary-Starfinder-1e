7249ca9c1431924e8473f84b4e16bde753cdbfca
ietzel <67884277+ietzel@users.noreply.github.com>
2025-04-16 11:10:57 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,5 +8,6 @@ class Main {
     System.out.println("Score: " + score);
     System.out.println("There are many questions related to the crash landing which led to a sector civilization on an unexplored planet.");
     System.out.println("Mysterious legends exist, such as of a desert with a primitive charred wood enclosure.");
+    System.out.println("Coastal lifeforms speak of nature disturbed by the mystery.");
   }
 }
